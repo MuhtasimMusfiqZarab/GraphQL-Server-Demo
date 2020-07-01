fbe7bd88c63ef2bfd7a4c12c601b290659123d4d
zarab <musfiqzarab@iut-dhaka.edu>
2020-07-01 16:49:04 +0600

    adding root queries & display graphiQL on the screen

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,30 @@
 //contains all the knowledge required for telling graphql exactly what your app data looks like(what properties each object has and how each object is related to each other)
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
   //this instructs graphql about the presence of a user in our app(an idea of the user)
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = graphql;
 
+//here are the hard coded list of users
+const users = [
+  {
+    id: '23',
+    firstName: 'Bill',
+    age: 20,
+  },
+  {
+    id: '47',
+    firstName: 'Samantha',
+    age: 21,
+  },
+];
+
 //UserType object instructs grpahql about what a user object looks like
+//type of data which is UserType
 const UserType = new GraphQLObjectType({
   //1st two are qruied properties
   name: 'User',
@@ -16,4 +33,31 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+// we need to give graphql a root query which allows us to jump in to a specific node in our graph of data
+//example (give the user of the id of 23)
+//entry point of our application/data
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      // the resolve function is where we actually go to our database & find the data we are looking for
+      //parentvalue - not ever used //arg is what we care about
+      resolve(parentValue, args) {
+        //return user with the given id inside the args
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+//GraphQLSchema takes in a root query & returns a graphl schema instance
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //glue layer between graphQl and express
 const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 const app = express();
 
 //registering graphql with express -
@@ -8,6 +9,8 @@ app.use(
   '/graphql',
   // has configaration object
   expressGraphQL({
+    //we need to put schema which is mandatory
+    schema,
     //graphiql is a development tool that allows us to make queries against ou development server
     graphiql: true,
   })
